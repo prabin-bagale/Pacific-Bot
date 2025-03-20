45720579bd011064a7fbeb629ae1c2824700c862
prabin bagale <prabinbagale08@gmail.com>
2025-03-20 13:25:59 +0545

    generate bot response

--- src/components/Chatform.jsx
+++ src/components/Chatform.jsx
@@ -16,10 +16,10 @@ const Chatform = ({ setChatHistory, generateBotResponse, chatHistory }) => {
   setTimeout(()=> {
     setChatHistory((history) => [...history, { role: "model", text: "Thinking..." }]);
     // Call generateBotResponse after 600ms delay
-    generateBotResponse([...chatHistory,{ role: "user", text: userMessage }]);
+    generateBotResponse([...chatHistory,{ role: "user", text: `Using the details provided above , please address this query: ${userMessage}`}]);
   },600);
 
-   
+}
 
 
   return (
