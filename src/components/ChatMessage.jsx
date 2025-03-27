fea63415cc9c8b2d2ae106804e6803defbc2d099
prabin bagale <prabinbagale08@gmail.com>
2025-03-27 23:06:15 +0545

    add timestamp feature

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,14 +1,16 @@
-
 import ChatbotIcon from "./ChatbotIcon";
 const ChatMessage = ({chat}) => {
+  const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  
   return (
     !chat.hideInChat && (
       <div className={`message ${chat.role === "model" ? "bot": "user" }-message ${chat.isError ? "error": ""}`}>     
-      {chat.role === "model" && <ChatbotIcon/>}
-      <p className='message-text'>
-      {chat.text}
-      </p>
-    </div>
+        {chat.role === "model" && <ChatbotIcon/>}
+        <p className='message-text'>
+          {chat.text}
+        </p>
+        <span className="message-timestamp">{timestamp}</span>
+      </div>
     )
   )
 }
