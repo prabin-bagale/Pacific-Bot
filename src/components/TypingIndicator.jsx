dadcc9165acbf141fc1388c8edcbff49a1457cc1
prabin bagale <prabinbagale08@gmail.com>
2025-03-27 23:07:18 +0545

    add typingindicator

---
+++ src/components/TypingIndicator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TypingIndicator = () => {
+  return (
+    <div className="typing-indicator">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+};
+
+export default TypingIndicator; 
